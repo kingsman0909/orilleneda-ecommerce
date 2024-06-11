686f48de89a6ceaae4673c14fdcce24eca47090f
kingsman0909 <75459928+kingsman0909@users.noreply.github.com>
2024-06-11 19:43:56 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,297 @@
+function topS(){
+    var body1 = document.getElementById("body1");
+    var body2 = document.getElementById("body2");
+    body1.style.display="none";
+    body2.style.display="block";
+}
+
+function home(){
+    var body1 = document.getElementById("body1");
+    var body2 = document.getElementById("body2");
+    body1.style.display="block";
+    body2.style.display="none";
+}
+
+function back(){
+    var body1 = document.getElementById("body1");
+    var body2 = document.getElementById("body2");
+    var b3 = document.getElementById("body3");
+    var body3 = document.getElementById("body4");
+    var b5 = document.getElementById("body5");
+    
+    body1.style.display="block";
+    body2.style.display="none";
+    b3.style.display="none";
+    body4.style.display="none";
+    b5.style.display="none";
+}
+
+function box1(){
+    var box = document.getElementById("box1");
+    box.style.display="block";
+}
+function boxOut1(){
+    var box = document.getElementById("box1");
+    box.style.display="none";
+}
+
+function box2(){
+    var box = document.getElementById("box2");
+    box.style.display="block";
+}
+function boxOut2(){
+    var box = document.getElementById("box2");
+    box.style.display="none";
+}
+
+function box3(){
+    var box = document.getElementById("box3");
+    box.style.display="block";
+}
+function boxOut3(){
+    var box = document.getElementById("box3");
+    box.style.display="none";
+}
+
+function box4(){
+    var box = document.getElementById("box4");
+    box.style.display="block";
+}
+function boxOut4(){
+    var box = document.getElementById("box4");
+    box.style.display="none";
+}
+
+function box5(){
+    var box = document.getElementById("box5");
+    box.style.display="block";
+}
+function boxOut5(){
+    var box = document.getElementById("box5");
+    box.style.display="none";
+}
+
+function box6(){
+    var box = document.getElementById("box6");
+    box.style.display="block";
+}
+function boxOut6(){
+    var box = document.getElementById("box6");
+    box.style.display="none";
+}
+
+
+
+function s1(){
+    var s1 = document.getElementById("s1");
+    s1.style.display="inline-block";
+}
+
+function s2(){
+    var s1 = document.getElementById("s1");
+    s1.style.display="inline-block";
+    var s2 = document.getElementById("s2");
+    s2.style.display="inline-block";
+}
+
+function s3(){
+    var s1 = document.getElementById("s1");
+    s1.style.display="inline-block";
+    var s2 = document.getElementById("s2");
+    s2.style.display="inline-block";
+    var s3 = document.getElementById("s3");
+    s3.style.display="inline-block";
+}
+
+function s4(){
+    var s1 = document.getElementById("s1");
+    s1.style.display="inline-block";
+    var s2 = document.getElementById("s2");
+    s2.style.display="inline-block";
+    var s3 = document.getElementById("s3");
+    s3.style.display="inline-block";
+    var s4 = document.getElementById("s4");
+    s4.style.display="inline-block";
+}
+
+function s5(){
+    var s1 = document.getElementById("s1");
+    s1.style.display="inline-block";
+    var s2 = document.getElementById("s2");
+    s2.style.display="inline-block";
+    var s3 = document.getElementById("s3");
+    s3.style.display="inline-block";
+    var s4 = document.getElementById("s4");
+    s4.style.display="inline-block";
+    var s5 = document.getElementById("s5");
+    s5.style.display="inline-block";
+}
+
+function show1(){
+    var d1 = document.getElementById("details1");
+    d1.style.display="inline-block";
+}
+function out1(){
+    var d1 = document.getElementById("details1");
+    d1.style.display="none";
+}
+
+function show2(){
+    var d1 = document.getElementById("details2");
+    d1.style.display="inline-block";
+}
+function out2(){
+    var d1 = document.getElementById("details2");
+    d1.style.display="none";
+}
+
+function show3(){
+    var d1 = document.getElementById("details3");
+    d1.style.display="inline-block";
+}
+function out3(){
+    var d1 = document.getElementById("details3");
+    d1.style.display="none";
+}
+
+function show4(){
+    var d1 = document.getElementById("details4");
+    d1.style.display="inline-block";
+}
+function out4(){
+    var d1 = document.getElementById("details4");
+    d1.style.display="none";
+}
+
+function show5(){
+    var d1 = document.getElementById("details5");
+    d1.style.display="inline-block";
+}
+function out5(){
+    var d1 = document.getElementById("details5");
+    d1.style.display="none";
+}
+
+function show6(){
+    var d1 = document.getElementById("details6");
+    d1.style.display="inline-block";
+}
+function out6(){
+    var d1 = document.getElementById("details6");
+    d1.style.display="none";
+}
+
+function shade1(){
+    var sf1 = document.getElementById("starF1");
+    sf1.style.display="inline-block";
+}
+function shade2(){
+    var sf1 = document.getElementById("starF1");
+    sf1.style.display="inline-block";
+    sf2 = document.getElementById("starF2");
+    sf2.style.display="inline-block";
+}
+function shade3(){
+    var sf1 = document.getElementById("starF1");
+    sf1.style.display="inline-block";
+    sf2 = document.getElementById("starF2");
+    sf2.style.display="inline-block";
+    sf3 = document.getElementById("starF3");
+    sf3.style.display="inline-block";
+}
+function shade4(){
+    var sf1 = document.getElementById("starF1");
+    sf1.style.display="inline-block";
+    sf2 = document.getElementById("starF2");
+    sf2.style.display="inline-block";
+    sf3 = document.getElementById("starF3");
+    sf3.style.display="inline-block";
+    sf4 = document.getElementById("starF4");
+    sf4.style.display="inline-block";
+}
+function shade5(){
+    var sf1 = document.getElementById("starF1");
+    sf1.style.display="inline-block";
+    sf2 = document.getElementById("starF2");
+    sf2.style.display="inline-block";
+    sf3 = document.getElementById("starF3");
+    sf3.style.display="inline-block";
+    sf4 = document.getElementById("starF4");
+    sf4.style.display="inline-block";
+    sf5 = document.getElementById("starF5");
+    sf5.style.display="inline-block";
+}
+
+function shaded4(){
+    var sf1 = document.getElementById("starF1");
+    sf1.style.display="inline-block";
+    sf2 = document.getElementById("starF2");
+    sf2.style.display="inline-block";
+    sf3 = document.getElementById("starF3");
+    sf3.style.display="inline-block";
+    sf4 = document.getElementById("starF4");
+    sf4.style.display="inline-block";
+    sf5 = document.getElementById("starF5");
+    sf5.style.display="none";
+}
+
+function shaded3(){
+    var sf1 = document.getElementById("starF1");
+    sf1.style.display="inline-block";
+    sf2 = document.getElementById("starF2");
+    sf2.style.display="inline-block";
+    sf3 = document.getElementById("starF3");
+    sf3.style.display="inline-block";
+    sf4 = document.getElementById("starF4");
+    sf4.style.display="none";
+    sf5 = document.getElementById("starF5");
+    sf5.style.display="none";
+    
+}
+
+function shaded2(){
+    var sf1 = document.getElementById("starF1");
+    sf1.style.display="inline-block";
+    sf2 = document.getElementById("starF2");
+    sf2.style.display="inline-block";
+    sf3 = document.getElementById("starF3");
+    sf3.style.display="none";
+    sf4 = document.getElementById("starF4");
+    sf4.style.display="none";
+    sf5 = document.getElementById("starF5");
+    sf5.style.display="none";
+}
+
+function shaded1(){
+    var sf1 = document.getElementById("starF1");
+    sf1.style.display="inline-block";
+    sf2 = document.getElementById("starF2");
+    sf2.style.display="none";
+    sf3 = document.getElementById("starF3");
+    sf3.style.display="none";
+    sf4 = document.getElementById("starF4");
+    sf4.style.display="none";
+    sf5 = document.getElementById("starF5");
+    sf5.style.display="none";
+}
+
+function affI(){
+    var b3 = document.getElementById("body3");
+    b3.style.display="block";
+    var b1 = document.getElementById("body1");
+    b1.style.display="none";
+}
+
+function promD(){
+    var b4 = document.getElementById("body4");
+    b4.style.display="block";
+    var b1 = document.getElementById("body1");
+    b1.style.display="none";
+}
+
+function menI(){
+    var b5 = document.getElementById("body5");
+    b5.style.display="block";
+    var b1 = document.getElementById("body1");
+    b1.style.display="none";
+}
